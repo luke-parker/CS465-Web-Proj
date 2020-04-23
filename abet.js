532facd2d87a6d827126e4c5f40eee6808c749bf
Todd Allen <tca861@gmail.com>
2020-04-22 22:03:45 -0400

    pressing "new +" adds a new row now

---
+++ abet.js
@@ -0,0 +1,14 @@
+var row = `
+<tr class="plan_row">
+    <td width="5%"><input class="i" type="number" min="0" max="100" placeholder="1"></td>
+    <td width="60%"><textarea class="i txt" maxlength="400" rows="4"></textarea></td>
+    <td width="10%"><div class="trash"></div></td>
+</tr>
+`;
+
+$(document).ready(function() {
+    $(".bttn, .new").click(function() {
+        $("#assessment tr:last").after(row);
+        console.log("tet")
+    });
+});
