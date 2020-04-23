7acc77eb1951398a248c36c6eb2a5f00fd73ca4f
Luke Parker <32403253+luke-parker@users.noreply.github.com>
2020-04-23 13:11:52 -0500

    update password.php

--- password.php
+++ password.php
@@ -3,22 +3,23 @@
     // include "nav.php";
 ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="abet.js"></script>
-        <link rel="stylesheet" type="text/css" href="abet.css">
-        <title>UTK ABET</title>
-    </head>
+  <head>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="abet.js"></script>
+      <link rel="stylesheet" type="text/css" href="abet.css">
+      <title>UTK ABET</title>
+  </head>
 
-    <div class="info_box">
+  <body>
+    <?php include "nav.php"; ?>
+      <div class="info_box">
 
-    </div>
+      </div>
 
-    <div class="reset_box">
+      <div class="reset_box">
 
-    </div>
-
+      </div>
     </div>
   </body>
+
 </html>
