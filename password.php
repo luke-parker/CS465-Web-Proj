dc864c1f1fc11a2bf050a96ce6d9167a87bf9d78
Luke Parker <32403253+luke-parker@users.noreply.github.com>
2020-04-23 13:09:31 -0500

    add header to password.html

--- password.php
+++ password.php
@@ -1,7 +1,15 @@
 <?php
     session_start();
-    include "nav.php";
+    // include "nav.php";
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="abet.js"></script>
+        <link rel="stylesheet" type="text/css" href="abet.css">
+        <title>UTK ABET</title>
+    </head>
 
     <div class="info_box">
 
