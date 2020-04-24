6bcbc73cbbe97a63000e5d2e6f449640dc0571c8
Luke Parker <32403253+luke-parker@users.noreply.github.com>
2020-04-23 20:37:48 -0500

    refactor back lol

--- password.php
+++ password.php
@@ -7,7 +7,32 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="password.css">
         <title>UTK ABET</title>
-        <script src="password.js"></script>
+        <script type="text/javascript">
+            document.getElementById("submit").onclick = function() {
+                console.log("submitting password");
+                var newPassword = document.getElementById("newPassword").value;
+                if (document.getElementById("confirmPassword").value != newPassword) {
+                    document.getElementById("passwordError").style.display = 'block';
+                } else {
+                    sendPasswordReset();
+                }
+            };
+
+            function sendPasswordReset(password) {
+                var xhttp = new XMLHttpRequest();
+
+                var emailQuery = "email=" + encodeURIComponent($_SESSION["email"]);
+                var passwordQuery = "password=" + encodeURIComponent(newPassword);
+
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var result = JSON.parse(this.responseText);
+                        document.getElementById("passwordError").style.display = 'none';
+                        document.getElementById("passwordSucceeded").style.display = 'block';
+                    }
+                }
+            }
+        </script>
     </head>
 
     <body>
